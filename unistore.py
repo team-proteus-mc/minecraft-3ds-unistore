3fe509eae27a8c7d484fae371d6b9d99b2cf99c5
susbaconhairman <111815397+susbaconhairman@users.noreply.github.com>
2023-07-18 10:26:50 -0500

    Create unistore.py

---
+++ unistore.py
@@ -0,0 +1,9 @@
+# UniStore maker, by skribbler#1095
+
+num = input("Enter the number 1 to make a new UniStore. Enter the number 2 to add an item to an already-existing one.")
+
+if num == "1":
+    print("Sorry, this feature is not available yet.")
+    
+if num == "2":
+    print("Test")
